0d21db629c0f9bb01f869783047fbea8b065095b
Philip Ross <dev@baseballlover723.com>
2024-03-10 08:06:06 -0700

    added a basic layout and header and footer

---
+++ app/javascript/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "././Header/Header";
+import Footer from "././Footer/Footer";
+
+export default (props) => (
+    <>
+      <div className="vw-100 vh-100 primary-color d-flex flex-column">
+      <Header />
+        <div className="container secondary-color">
+          {props.children}
+        </div>
+      <Footer />
+      </div>
+    </>
+);
